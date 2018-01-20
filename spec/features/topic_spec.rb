de138c0f5a3f8f2b07424c104aee3a4869e2b496
ericash0923 <eric@gaglani.com>
2018-01-20 00:11:11 +0000

    all test for topic is set for crud function

--- spec/features/topic_spec.rb
+++ spec/features/topic_spec.rb
@@ -3,48 +3,66 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  describe 'index' do
-    before do
-      @topic = Topic.create!(title: "First Topic")
-      @sub_topic = Topic.create!(title: "Sub Topic", parent: @topic)
-      visit root_path
-    end
-    
-    it 'can be reached successfully' do
-      expect(page.status_code).to eq(200)
-    end
-    
-    it 'has parent node' do
-      expect(page).to have_content(/First Topic/)
-    end
-    
-    it 'does not have any sub_topic' do
-      expect(page).to_not have_content(/Sub Topic/)
-    end
-  end
-end
-
-describe 'creation' do
   before do
     @topic = Topic.create!(title: "First Topic")
     @sub_topic = Topic.create!(title: "Sub Topic", parent: @topic)
   end
-    
-  describe 'new' do
-    it 'has a new form that can be reached' do
-      visit new_topic_path
-      expect(page.status_code).to eq(200)
-    end
   
-    it 'can be created from new form page' do
-      visit new_topic_path
-      fill_in 'topic[title]', with: 'Some topic'
-      expect { click_on "Create Topic"}.to change(Topic, :count).by(1)
+    describe 'index' do
+      before do
+        visit root_path
+      end
+      
+      it 'can be reached successfully' do
+        expect(page.status_code).to eq(200)
+      end
+      
+      it 'has parent node' do
+        expect(page).to have_content(/First Topic/)
+      end
+      
+      it 'does not have any sub_topic' do
+        expect(page).to_not have_content(/Sub Topic/)
+      end
+    end
+
+    describe 'creation' do
+      before do
+        visit new_topic_path
+      end
+      
+      it 'has a new form that can be reached' do
+        expect(page.status_code).to eq(200)
+      end
+  
+      it 'can be created from new form page' do
+        fill_in 'topic[title]', with: 'Some topic'
+        
+        expect { click_on "Create Topic"}.to change(Topic, :count).by(1)
+      end
+    
+      it 'id pass into new action' do
+        visit topic_path(@topic)
+        expect(find_link('New topic')[:href]).to eq("/topics/new?parent_id=#{@topic.id}")
+      end
     end
     
-    it 'id pass into new action' do
-      visit topic_path(@topic)
-      expect(find_link('New topic')[:href]).to eq("/topics/new?parent_id=#{@topic.id}")
+    describe 'edit' do
+      it 'can be edited' do
+        visit edit_topic_path(@topic)
+        fill_in 'topic[title]', with: 'Edit topic title'
+        click_on "Update Topic"
+        
+        expect(page).to have_content("Edit topic title")
+      end
     end
-  end
+    
+    describe 'delete' do
+      it 'topic can be deleted' do
+        topic_delete = Topic.create(title: "Delete Topic")
+        visit root_path
+        
+        expect { click_on("delete_topic_#{topic_delete.id}_from_index") }.to change(Topic, :count).by(-1)
+      end
+    end
 end
