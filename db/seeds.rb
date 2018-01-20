ca785764c2b557716a7e53d12794e3782f6610c8
ericash0923 <eric@gaglani.com>
2018-01-20 12:19:57 +0000

    add doc to topic show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,21 @@
  Topic.create!(title: "Topic #{topic}")
  
   2.times do |subtopic|
-   Topic.create!(title: "SubTopic #{subtopic}", parent_id: (topic + 1))
-
-    1.times do |three|
-     Topic.create!(title: "SubTopic #{three}", parent_id: (subtopic + 1))
-    end
+   Topic.create!(title: "SubTopic #{subtopic + 1}", parent_id: (topic + 1))
   end
 end
 
-puts "5 root, subfolder and third level folder"
+puts "5 root, subfolder"
+
+5.times do  |doc|
+ Document.create!(name: "Doc #{doc}", topic_id: 1)
+end
+
+puts "5 doc for root folder"
+
+5.times do  |doc|
+ Document.create!(name: "Doc #{doc}", topic_id: 2)
+end
+
+puts "5 doc for subfolder"
+
