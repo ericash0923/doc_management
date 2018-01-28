0d6d2a7e7ac2513f221843e8153712f8d17b3b88
ericash0923 <eric@gaglani.com>
2018-01-28 21:28:48 +0000

    setup admin doc

---
+++ app/controllers/admin/documents_controller.rb
@@ -0,0 +1,32 @@
+class Admin::DocumentsController < Admin::AdminController
+  before_action :set_document, only: [:destroy]
+  
+  def new
+    @admin_document = Document.new
+  end
+  
+  def create
+    @admin_document = Document.new(document_params)
+    
+    if @admin_document.save
+      redirect_to [:admin, @admin_topic], notice: "Your document #{@admin_document.name} has been uploader"
+    else 
+      render :new
+    end
+  end
+  
+  def destroy
+    @admin_document.destroy
+    redirect_to [:admin, @admin_topic], notice: "Your #{@admin_document.name} was deleted successfully"
+  end
+  
+  private
+  
+  def set_document
+    @admin_document = Document.find(params[:id])
+  end
+  
+  def document_params
+    params.require(:document).permit(:name, :attachment, :topic_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   
   namespace :admin do
     resources :topics
+    resources :documents
   end
   
   root to: 'topics#index'
