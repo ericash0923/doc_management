1e275fd406518f980efe498dbfe873e0a1940da7
ericash0923 <eric@gaglani.com>
2018-02-07 19:53:47 +0000

    add enum status to topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,8 @@ class Topic < ApplicationRecord
   include Tree
   has_ancestry
   
+  enum status: { admin_only: 0, non_admin: 1 }
+  
   has_many :documents, dependent: :destroy
   validates_presence_of :title
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124231414) do
+ActiveRecord::Schema.define(version: 20180207193816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,10 @@ ActiveRecord::Schema.define(version: 20180124231414) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "ancestry"
+    t.integer  "status",     default: 0
     t.index ["ancestry"], name: "index_topics_on_ancestry", using: :btree
   end
 
